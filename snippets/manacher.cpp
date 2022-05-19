b09a40eaae83bc54250f38ee719530ffe1b8231b
mateus sze cosenza <80402245+cosenza987@users.noreply.github.com>
2022-05-18 22:00:23 -0300

    Update manacher.cpp

--- snippets/manacher.cpp
+++ snippets/manacher.cpp
@@ -1,25 +1,25 @@
-bool mc(string s) {
-    string t = "$#";
-    for(int i = 0; i < s.size(); i++) {
-        t += s[i];
-        t += '#';
-    }
-    vector<int> p(t.size());
-    int mx = 0, id = 0;
-    for(int i = 1; i < t.size(); i++) {
-        p[i] = (mx > i ? min(p[2 * id - i], mx - i) : 1);
-        while(t[i + p[i]] == t[i - p[i]]) {
-            p[i]++;
-        }
-        if(mx < i + p[i]) {
-            mx = i + p[i];
-            id = i;
-        }
-    }
-    for(int i = 1; i < t.size(); i++) {
-        if(((p[i] - 1) % 2) == (m % 2) and (p[i] - 1) >= m)  {
-            return true;
-        }
-    }
-    return false;
+// Manacher - O(n) (do navarro)
+
+// d1 -> odd : size = 2*d1[i] - 1
+// d2 -> even : size = 2*d2[i]
+
+
+vector<int> d1, d2;
+
+void manacher(string &s) {
+  int n = s.size();
+  d1.resize(n), d2.resize(n);
+  for(int i = 0, l1 = 0, l2 = 0, r1 = -1, r2 = -1; i < n; i++) {
+    if(i <= r1) d1[i] = min(d1[r1+l1-i], r1-i+1);
+    if(i <= r2) d2[i] = min(d2[r2+l2-i+1], r2-i+1);
+    
+    while(i - d1[i] >= 0 and i + d1[i] < n and 
+        s[i - d1[i]] == s[i + d1[i]]) d1[i]++;
+    
+    while(i - d2[i] - 1 >= 0 and i + d2[i] < n and 
+        s[i - d2[i] - 1] == s[i + d2[i]]) d2[i]++;
+
+    if(i + d1[i] - 1 > r1) l1 = i - d1[i] + 1, r1 = i + d1[i] - 1;
+    if(i + d2[i] - 1 > r2) l2 = i - d2[i], r2 = i + d2[i] - 1;
+  }
 }
