879ac842a58d816ae30a9755a62ebce5671f08a0
mateus sze cosenza <80402245+cosenza987@users.noreply.github.com>
2022-12-14 18:31:10 -0300

    Update seg_tree.cpp

--- useful structs/seg_tree.cpp
+++ useful structs/seg_tree.cpp
@@ -2,13 +2,16 @@ struct seg_tree {
     //modify accordingly
     int n;
     vector<long long> v, lz, st;
-    void build(int p = 1, int l = 1, int r = n) {
-        if(l == r) {
+    void build() {
+        build(1, 1, n);
+    }
+    void build(int p, int l, int r) {
+        if (l == r) {
             st[p] = v[l];
             return;
         }
         build(2 * p, l, (l + r) >> 1);
-        build(2 * p + 1, (l + r) >> 1 + 1, r);
+        build(2 * p + 1, ((l + r) >> 1) + 1, r);
         st[p] = st[2 * p] + st[2 * p + 1];
     }
     seg_tree(int n_) {
@@ -18,45 +21,52 @@ struct seg_tree {
         v.resize(n + 1);
         build();
     }
-    seg_tree(vector<long long> &v_) {
+    seg_tree(vector<long long>& v_) {
         v = v_;
         n = v.size();
+        n--;
         lz.resize(4 * n);
         st.resize(4 * n);
         build();
     }
     void push(int p, int l, int r) {
-        if(lz[p]) {
-            st[p] = lz[p];
-            if(l != r) {
-                lz[2 * p] = lz[p];
-                lz[2 * p + 1] = lz[p];
+        if (lz[p]) {
+            st[p] += (r - l + 1) * lz[p];
+            if (l != r) {
+                lz[2 * p] += lz[p];
+                lz[2 * p + 1] += lz[p];
             }
             lz[p] = 0;
         }
     }
-    long long query(int i, int j, int p = 1, int l = 1, int r = n) {
+    long long query(int i, int j) {
+        return query(i, j, 1, 1, n);
+    }
+    long long query(int i, int j, int p, int l, int r) {
         push(p, l, r);
-        if(l > j or r < i) {
+        if (l > j or r < i) {
             return 0;
         }
-        if(l >= i and j >= r) {
+        if (l >= i and j >= r) {
             return st[p];
         }
-        return query(i, j, 2 * p, l, (l + r) >> 1) + query(i, j, 2 * p + 1, (l + r) >> 1 + 1, r);
+        return query(i, j, 2 * p, l, (l + r) >> 1) + query(i, j, 2 * p + 1, ((l + r) >> 1) + 1, r);
     }
-    void update(int i, int j, int v, int p = 1, int l = 1, int r = n) {
+    void update(int i, int j, int v) {
+        update(i, j, v, 1, 1, n);
+    }
+    void update(int i, int j, int v, int p, int l, int r) {
         push(p, l, r);
-        if(l > j or r < i) {
+        if (l > j or r < i) {
             return;
         }
-        if(l >= i and j >= r) {
-            lz[p] = v;
+        if (l >= i and j >= r) {
+            lz[p] += v;
             push(p, l, r);
             return;
         }
         update(i, j, v, 2 * p, l, (l + r) >> 1);
-        update(i, j, v, 2 * p + 1, (l + r) >> 1 + 1, r);
+        update(i, j, v, 2 * p + 1, ((l + r) >> 1) + 1, r);
         st[p] = st[2 * p] + st[2 * p + 1];
     }
 };
