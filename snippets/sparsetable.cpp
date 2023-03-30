7c89d3610412668ceaba27af5610697f6aca10a4
mateus sze cosenza <80402245+cosenza987@users.noreply.github.com>
2023-03-29 23:51:14 -0300

    Update sparsetable.cpp

--- snippets/sparsetable.cpp
+++ snippets/sparsetable.cpp
@@ -7,7 +7,7 @@ void bst(int n) {
     for(int i = 0; i < n; i++) {
         table[i][0] = v[i];
     }
-    for(int j = 1; j <= log2(n); j++) {
+    for(int j = 1; j <= 31 - __builtin_clz(n); j++) {
         for(int i = 0; i <= n - (1 << j); i++) {
             table[i][j] = __gcd(table[i][j - 1], table[i + (1 << (j - 1))][j - 1]);
         }
@@ -15,6 +15,6 @@ void bst(int n) {
 }
 
 int query(int l, int r) {
-    int j = (int)log2(r - l + 1);
+    int j = 31 - __builtin_clz(r - l + 1);
     return __gcd(table[l][j], table[r - (1 << j) + 1][j]);
 }
